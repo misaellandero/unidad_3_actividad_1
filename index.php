e25eccfd9bd6d4664cafaf8db79845f470812ba5
misaellandero <misaellandero@landercorp.com.mx>
2018-04-26 01:03:55 -0500

    se creo una nueva ruta get clima, que regresa el nombre que suministras y mi numero de la unam

--- index.php
+++ index.php
@@ -4,11 +4,12 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+$clima= new \Slim\App;
+$clima->get('/clima/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+    $cuentaUnam = '415026442';
+    $response->getBody()->write("Hola mi nombre es, $name y mi numero de cuenta de la unam es: $cuentaUnam");
 
     return $response;
 });
-$app->run();
+$clima->run();
