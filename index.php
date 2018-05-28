9c5000bdb12d8b1d7eb91f3243cd43e453832f58
misaellandero <misaellandero@landercorp.com.mx>
2018-05-28 11:11:39 -0500

    respuesta del clima en una ciudad por id

--- index.php
+++ index.php
@@ -2,16 +2,23 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+use GuzzleHttp\Client;
+
+
 require 'vendor/autoload.php';
 
 $clima= new \Slim\App;
-$clima->get('/clima/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $cuentaUnam = '415026442';
+$clima->get('/clima/{id_ciudad}', function (Request $request, Response $response, array $args) {
 
-    $datos = array('nombre' => $name, 'cuetaUnam' => $cuentaUnam);
+    $cliente = new Client();
+    $id_ciudad = $args['id_ciudad'];
+    $appiKey = '27e64a82f4947a495eef42e955f4c651';
 
-    return $response->withJson($datos);
- 
+    $url = "api.openweathermap.org/data/2.5/weather?id=$id_ciudad&appid=$appiKey&units=metric";
+    $respuesta = $cliente->get($url);
+    $body =  $respuesta->getBody();
+
+    return $respuesta;
+
 });
 $clima->run();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,6 +8,9 @@ class ComposerStaticInit32f134527e15b41747598df528a07612
 {
     public static $files = array (
         '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
+        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -23,6 +26,12 @@ class ComposerStaticInit32f134527e15b41747598df528a07612
         'I' => 
         array (
             'Interop\\Container\\' => 18,
+        ),
+        'G' => 
+        array (
+            'GuzzleHttp\\Psr7\\' => 16,
+            'GuzzleHttp\\Promise\\' => 19,
+            'GuzzleHttp\\' => 11,
         ),
         'F' => 
         array (
@@ -46,6 +55,18 @@ class ComposerStaticInit32f134527e15b41747598df528a07612
         'Interop\\Container\\' => 
         array (
             0 => __DIR__ . '/..' . '/container-interop/container-interop/src/Interop/Container',
+        ),
+        'GuzzleHttp\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+        ),
+        'GuzzleHttp\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
+        ),
+        'GuzzleHttp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
         ),
         'FastRoute\\' => 
         array (
